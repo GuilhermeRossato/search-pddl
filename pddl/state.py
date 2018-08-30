7d418c1ea1207b2b487150cf6300b11eed663dbc
Guilherme Rossato <guilhermrossato@gmail.com>
2018-08-30 17:01:57 -0300

    Adicionado comentários menos inúteis aos metodos de estado

--- pddl/state.py
+++ pddl/state.py
@@ -1,5 +1,8 @@
 #-----------------------------------------------
 # Applicable
+#
+# Check if the positive and negative preconditions are met in a given state
+# @return bool
 #-----------------------------------------------
 
 def applicable(state, positive, negative):
@@ -7,6 +10,9 @@ def applicable(state, positive, negative):
 
 #-----------------------------------------------
 # Apply
+#
+# Given a state, apply the positive and negative effects to it
+# @return frozenset A new state, modified by the effects given from the parameters
 #-----------------------------------------------
 
 def apply(state, positive, negative):
